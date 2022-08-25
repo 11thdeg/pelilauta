4e94b05631a098b55d8245504d6f0a4d987f636c
Ville Takanen <ville.takanen@futurice.com>
2022-08-25 15:03:03 +0300

    feat: added 11th libs

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import '@11thdeg/cyan/style.css'
 
 createApp(App).mount('#app')
+
+// Add Cyan/Charna Theming to body
+document.body.classList.add('cyan--themed')
