7d2717bd21b4caf5b7c83e295a5466416f1bf50f
Ville Takanen <ville.takanen@futurice.com>
2022-08-30 13:23:40 +0300

    feat: account data ux done

--- src/components/account/AccountDataPane.vue
+++ src/components/account/AccountDataPane.vue
@@ -1,9 +1,17 @@
 <script lang="ts" setup>
 import { useI18n } from 'vue-i18n'
 import { useSession } from '../../composables/useSession'
+import { logError } from '../../utils/logHelpers'
 
 const { t } = useI18n()
 const { account, active } = useSession()
+
+function setProfilePhoto (url: string) {
+  logError(`makePhotoActive: ${url}`, 'not implemented')
+}
+function setNickname (nickname: string) {
+  logError(`setNickname: ${nickname}`, 'not implemented')
+}
 </script>
 
 <template>
@@ -11,9 +19,46 @@ const { account, active } = useSession()
     <cyan-loader v-if="!active" />
     <template v-else>
       <h2>{{ t('account.accountDataPane.title') }}</h2>
-      <div class="code dontBreakOut">
-        {{ account }}
-      </div>
+      <p class="TypeBody2">{{ t('account.accountDataPane.description') }}</p>
+
+      <h3>{{ t('account.accountDataPane.session') }}</h3>
+      <section class="dataTable">
+        <!-- Email -->
+        <p><strong>{{ t('account.field.email') }}</strong></p>
+        <p>{{ account.email }}</p>
+        <!-- Name -->
+        <p><strong>{{ t('account.field.displayName') }}</strong></p>
+        <p>
+          {{ account.displayName }}
+          <cyan-button
+            :label="t('action.useInApp')"
+            text
+            @click="setNickname(account.displayName)"
+          />
+        </p>
+        <!-- photoURL -->
+        <p><strong>{{ t('account.field.photoURL') }}</strong></p>
+        <p>
+          <img
+            v-if="account.photoURL"
+            :src="account.photoURL"
+          >
+          <span v-else>{{ t('account.field.noPhoto') }}</span>
+          <cyan-button
+            v-if="account.photoURL"
+            :label="t('action.useInApp')"
+            text
+            @click="setProfilePhoto(account.photoURL)"
+          />
+        </p>
+      </section>
+
+      <h3>{{ t('account.accountDataPane.settings') }}</h3>
+      <section class="dataTable">
+        <!-- Lightmode -->
+        <p><strong>{{ t('account.field.lightMode') }}</strong></p>
+        <p>{{ account.lightMode }}</p>
+      </section>
     </template>
   </article>
 </template>
