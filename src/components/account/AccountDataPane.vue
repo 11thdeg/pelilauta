e74f85c7b2f7837eb0d71e42994e5e0416cb5bc4
Ville Takanen <ville.takanen@futurice.com>
2022-09-01 08:52:00 +0300

    fix: login info is now more informative

--- src/components/account/AccountDataPane.vue
+++ src/components/account/AccountDataPane.vue
@@ -1,64 +1,21 @@
 <script lang="ts" setup>
 import { useI18n } from 'vue-i18n'
 import { useSession } from '../../composables/useSession'
-import { logError } from '../../utils/logHelpers';
 import { toDisplayString } from '../../utils/toDisplayString'
 
 const { t } = useI18n()
 const { account, active, profile} = useSession()
-    
 
-function setNickname (nickname: string) {
-  logError(`setNickname: ${nickname}`, 'not implemented')
-}
 </script>
     
 <template>
   <article
     v-if="active"
-    class="AccountSettingsPane Column"
+    class="AccountSettingsPane Column dontBreakOut"
   >
     <h2>{{ t('settings.accountDataPane.title') }}</h2>
-    <section>
-      <h3>{{ t('settings.accountDataPane.ssoTitle') }}</h3>
-      <p class="lowEmphasis">
-        {{ t('settings.accountDataPane.ssoDescription') }}
-      </p>
-      <div class="dataTable">
-        <!-- uid -->
-        <p class="tableHead">
-          {{ t('account.field.uid') }}
-        </p>
-        <p>{{ account.uid }}</p>
+    
 
-        <!-- Email -->
-        <p class="tableHead">{{ t('account.field.email') }}</p>
-        <p>{{ account.email }}</p>
-        
-        <!-- Name -->
-        <p class="tableHead">{{ t('account.field.displayName') }}</p>
-        <p>
-          {{ account.displayName }}
-          <cyan-button
-            :label="t('action.useInApp')"
-            text
-            @click="setNickname(account.displayName)"
-          />
-        </p>
-      </div>
-      <p class="TypeBody2 lowEmphasis">
-        This data is not erased automatically, when you delete your profile, as it's not saved to the App data. The Authentication scrapped manually for 
-        users who's data is deleted from the platform.
-      </p>
-      <p class="TypeBody2 lowEmphasis">
-        More info on how Firestore saves the authentication data can be found here 
-        <a href="https://firebase.google.com/support/privacy">https://firebase.google.com/support/privacy</a>
-      </p>
-      <p class="TypeBody2 lowEmphasis">
-        More info on what data is saved from your logins, can be found on the source code responsible for the Account persistence: 
-        <a href="https://github.com/11thdeg/skaldstore/blob/main/src/Account.ts">https://github.com/11thdeg/skaldstore/blob/main/src/Account.ts</a>
-      </p>
-    </section>
     <section>
       <h4>App Metadata</h4>
       <p>This data is managed by the App, and stored to your private profile visible only to you and the administrative users</p>
@@ -96,6 +53,47 @@ function setNickname (nickname: string) {
           >{{ profile.avatarURL }}</a>
         </p>
       </div>
+    </section>
+
+    <!-- Login info -->
+    <section>
+      <h3>{{ t('settings.accountDataPane.ssoTitle') }}</h3>
+      <p class="lowEmphasis">
+        {{ t('settings.accountDataPane.ssoDescription') }}
+      </p>
+      <div class="dataTable">
+        <!-- uid -->
+        <p class="tableHead">
+          {{ t('fields.account.uid') }}
+        </p>
+        <p>{{ account.uid }}</p>
+
+        <!-- Email -->
+        <p class="tableHead">
+          {{ t('fields.account.email') }}
+        </p>
+        <p>{{ account.email }}</p>
+        
+        <!-- Name -->
+        <p class="tableHead">
+          {{ t('fields.account.displayName') }}
+        </p>
+        <p>
+          {{ account.displayName }}
+        </p>
+      </div>
+      <p class="TypeBody2 lowEmphasis">
+        This data is not erased automatically, when you delete your profile, as it's not saved to the App data. The Authentication scrapped manually for 
+        users who's data is deleted from the platform.
+      </p>
+      <p class="TypeBody2 lowEmphasis">
+        More info on how Firestore saves the authentication data can be found here 
+        <a href="https://firebase.google.com/support/privacy">https://firebase.google.com/support/privacy</a>
+      </p>
+      <p class="TypeBody2 lowEmphasis">
+        More info on what data is saved from your logins, can be found on the source code responsible for the Account persistence: 
+        <a href="https://github.com/11thdeg/skaldstore/blob/main/src/Account.ts">https://github.com/11thdeg/skaldstore/blob/main/src/Account.ts</a>
+      </p>
     </section>
   </article>
 </template>
