090ea06f39f26b01890019ef09cc5f7e3cef3bf2
Ville Takanen <ville.takanen@futurice.com>
2022-09-20 12:51:21 +0300

    fix: backported snacks and top bar stickynes

--- src/components/ui/TopBar.vue
+++ src/components/ui/TopBar.vue
@@ -1,19 +1,31 @@
 
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
 const props = defineProps<{
   title: string,
   sticky: boolean
 }>()
+
+const overlay = ref(false)
+onMounted(() => {
+  if (props.sticky) {
+    document.addEventListener('scroll', (e: Event) => {
+      const top = window.pageYOffset || (e.target as HTMLElement).scrollTop || 0
+      overlay.value = top > 2
+    })
+  }
+})
 </script>
 
 <template>
   <nav
     id="TopBar"
-    :class="{ sticky: sticky }"
+    :class="{ sticky: sticky, overlay: overlay }"
   >
     <cyan-icon
       noun="back"
-      class="clickable hoverable inAppBar"
+      class="clickable hoverable"
       @click="$router.back()"
     />
     <h1
@@ -36,6 +48,18 @@ const props = defineProps<{
   align-items: top
   height: 56px
   gap: 12px
-.inAppBar
-  margin-top: 8px
+  &.sticky
+    position: -webkit-sticky
+    position: sticky
+    top: 0px
+    background-color: white
+    z-index: 100000
+    transition: all 0.2s ease-in-out
+    margin-left: -8px
+    padding-left: 8px
+    &.overlay
+      transition: all 0.6s ease-in-out
+      box-shadow: 0px 0px 55px -12px var(--chroma-secondary-d)
+  cyan-icon
+    margin-top: 4px
 </style>
