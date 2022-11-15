73490c412400113f43e8173cb26bc854bcb5408e
Ville Takanen <ville.takanen@futurice.com>
2022-11-15 15:17:40 +0200

    fix: about page fixed

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,35 @@
 <script lang="ts" setup>
+import { Page } from '@11thdeg/skaldstore'
+import { onMounted, ref } from 'vue'
+import MarkdownSection from '../components/content/MarkdownSection.vue'
 import AppBar from '../components/navigation/AppBar.vue'
+import { fetchPage } from '../composables/usePages'
+
+const page = ref(new Page())
+
+onMounted(async () => {
+  page.value = await fetchPage('mekanismi', 'pelilauta-about') || new Page()
+})
 
 </script>
 <template>
   <main>
     <AppBar />
-    <h1>About Site Placeholder</h1>
+    <main
+      v-if="page.name"
+      class="singleColumnLayout"
+    >
+      <article class="Column">
+        <h1>{{ page.name }}</h1>
+        <MarkdownSection 
+          v-if="page.markdownContent"
+          :content="page.markdownContent"
+        />
+        <div
+          v-else
+          :innerHTML="page.htmlContent"
+        />
+      </article>
+    </main>
   </main>
 </template>
