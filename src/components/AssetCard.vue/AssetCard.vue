ee7e98e33a4a51995faa02406847525fc24030d9
Ville Takanen <ville.takanen@futurice.com>
2022-11-28 13:04:29 +0200

    fix: asset list uses cyan cards

---
+++ src/components/AssetCard.vue/AssetCard.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+const props = defineProps<{
+  to: string;
+  url: string;
+  name: string;
+  description: string;
+}>()
+
+const title = computed(() => (props.name ? props.name : '...').substring(0, 22))
+</script>
+
+<template>
+  <cyan-card
+    :cover="url"
+    elevation="1"
+  >
+    <h4
+      slot="title"
+      class="downscaled"
+    >
+      <router-link :to="to">
+        {{ title }}
+      </router-link>
+    </h4>
+    <cyan-icon
+      slot="avatar"
+      noun="assets"
+    />
+    <div class="assetData">
+      <p class="downscaled TypeBody2">
+        {{ name }}
+      </p>
+      <p class="downscaled TypeCaption">
+        {{ description }}
+      </p>
+    </div>
+  </cyan-card>
+</template>
+
+<style lang="sass" scoped>
+.assetData
+  margin-top: 24px
+</style>
--- src/components/assets/AssetListColumn.vue
+++ src/components/assets/AssetListColumn.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
 import { useAssets, assetName, assetDescription, assetLicense } from '../../composables/useSession/useAssets'
+import AssetCard from '../AssetCard.vue/AssetCard.vue'
 import AddAssetButton from './AddAssetButton.vue'
 
 const { t } = useI18n()
@@ -8,7 +9,7 @@ const { assets } = useAssets()
 
 </script>
 <template>
-  <article class="Column AssetListColumn double">
+  <article class="Column large">
     <cyan-toolbar>
       <cyan-toolbar-heading :level="2">
         {{ t('assets.list.title') }}
@@ -16,6 +17,19 @@ const { assets } = useAssets()
       <cyan-spacer />
       <AddAssetButton />
     </cyan-toolbar>
+    <section class="cardGrid">
+      <AssetCard
+        v-for="asset in assets"
+        :key="asset.key"
+        :url="asset.url || ''"
+        :to="`/assets/${asset.key}`"
+        :name="assetName(asset)"
+        :description="assetDescription(asset)"
+        :license="assetLicense(asset)"
+      />
+    </section>
+  </article>
+  <!--article class="Column AssetListColumn double">
     <section class="AssetList dataTable threeCols">
       <template
         v-for="asset in assets"
@@ -42,5 +56,14 @@ const { assets } = useAssets()
         <div><hr></div>
       </template>
     </section>
-  </article>
+  </article-->
 </template>
+
+<style lang="sass" scoped>
+.cardGrid
+  display: flex
+  flex-wrap: wrap
+  gap: 8px
+  cyan-card
+    width: calc(100% / 2 - 8px)
+</style>
