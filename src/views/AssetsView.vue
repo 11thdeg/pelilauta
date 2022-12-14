cbde03e2fd853aadf69b2ea05a2a7bb9e98ad025
Ville Takanen <ville.takanen@futurice.com>
2022-12-14 14:39:28 +0200

    chore: lints

--- src/views/AssetsView.vue
+++ src/views/AssetsView.vue
@@ -3,9 +3,9 @@ import AppBar from '../components/navigation/AppBar.vue'
 import AssetListColumn from '../components/assets/AssetListColumn.vue'
 import WithPermission from '../components/ui/WithPermission.vue'
 import { useSession } from '../composables/useSession'
-import { onMounted } from 'vue';
-import { useI18n } from 'vue-i18n';
-import { useTitle } from '@vueuse/core';
+import { onMounted } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { useTitle } from '@vueuse/core'
 
 const { anonymous } = useSession()
 const { t } = useI18n()
