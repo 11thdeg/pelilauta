723341868414a4110b2ea0f6129214ee86b850de
Ville Takanen <ville.takanen@futurice.com>
2023-01-27 13:16:39 +0200

    fix: added mekanismi card

---
+++ src/components/calltoaction/MyrrysCard/MyrrysCard.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+import MyrrysBlogExcerptSection from './MyrrysBlogExcerptSection.vue'
+</script>
+
+<template>
+  <cyan-card
+    elevation="1"
+    class="MyrrysCard"
+  >
+    <h4
+      slot="title"
+      class="myrrys"
+    >
+      Myrrys
+    </h4>
+    <cyan-icon
+      slot="avatar"
+      noun="myrrys"
+    />
+    <MyrrysBlogExcerptSection />
+
+    <cyan-toolbar>
+      <cyan-spacer />
+      <a
+        href="https://myrrys.com"
+        target="_blank"
+      ><button class="secondary">
+        <cyan-icon
+          noun="outlink"
+          small
+        />
+        Myrrys.com
+      </button></a>
+      <cyan-spacer />
+    </cyan-toolbar>
+  </cyan-card>
+</template>
+
+<style lang="sass">
+.MyrrysCard
+  background-image:  linear-gradient(hsla(var(--chroma-primary-a-hsl), 0.55), hsla(var(--chroma-secondary-a-hsl), 0.55)),url('/proprietary/images/letl_cover.jpg')
+  background-size: cover
+  background-position-y: center
+  background-position-x: right
+</style>
