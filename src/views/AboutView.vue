eb5229396dd9ad419d387b49a4c0d08ef4f0dd18
Ville Takanen <ville.takanen@futurice.com>
2023-06-13 15:13:11 +0300

    fix: removed old cyan from app bar

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,7 +3,6 @@ import { Page } from '@11thdeg/skaldstore'
 import { useTitle } from '../composables/useTitle'
 import { onMounted, ref, computed } from 'vue'
 import { useI18n } from 'vue-i18n'
-import AppBar from '../components/navigation/AppBar.vue'
 import SiteMetaDataColumn from '../components/SiteMetaDataColumn.vue'
 import WithLoader from '../components/ui/WithLoader.vue'
 import { useContentEntry } from '../composables/useContentEntry'
@@ -32,7 +31,14 @@ const content = computed(() => {
 
 <template>
   <div>
-    <AppBar :title="pageTitle" />
+    <!-- For the App front page, we use the app-bar defaults! -->
+    <cyan-top-app-bar>
+      <cyan-icon
+        class="noun onlyOnMobile"
+        noun="pelilauta"
+      />
+      <h3>{{ pageTitle }}</h3>
+    </cyan-top-app-bar>
     <main
       v-if="page.name"
       class="bookLayout"
