f421df5d6dcf10a50baa9a6d8b456bbef35d2331
Ville Takanen <ville.takanen@futurice.com>
2023-06-13 10:58:34 +0300

    fix: updated profile app-bar to modern

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
 import { Profile } from '@11thdeg/skaldstore'
 import { onMounted, Ref, ref } from 'vue'
-import AppBar from '../components/navigation/AppBar.vue'
 import { fetchProfile } from '../composables/useProfiles'
 import PublicProfileColumn from '../components/profiles/PublicProfileColumn.vue'
 import ProfilePostsColumn from '../components/profiles/ProfilePostsColumn.vue'
 import { useTitle } from '../composables/useTitle'
+import { useRouter } from 'vue-router'
+import ShareButton from '../components/ShareButton/ShareButton.vue'
 
 const props = defineProps<{
   uid: string
 }>()
+
+const router = useRouter()
 
 const profile:Ref<Profile|undefined> = ref(undefined)
 
@@ -22,7 +25,17 @@ onMounted(async () => {
 
 <template>
   <div id="ProfileView">
-    <AppBar />
+    <cyan-top-app-bar
+      sticky
+      modal
+      @back="router.back()"
+    >
+      <h3>
+        {{ profile?.nick || '...' }}
+      </h3>
+      <cyan-spacer />
+      <ShareButton />
+    </cyan-top-app-bar>
     <main class="bookLayout">
       <cyan-loader
         v-if="!profile"
