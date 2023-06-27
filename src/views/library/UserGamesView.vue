010cd91b47727d220b7fdf1fc72487ebe95a9566
Ville Takanen <ville.takanen@futurice.com>
2023-06-27 14:09:13 +0300

    fix: cn-app-bar refactors for library

--- src/views/library/UserGamesView.vue
+++ src/views/library/UserGamesView.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import FabTray from '../../components/FabTray/FabTray.vue'
 import LibraryGamesArticle from '../../components/library/LibraryNavigationTray/LibraryGamesArticle/LibraryGamesArticle.vue'
 import LibraryNavigationTray from '../../components/library/LibraryNavigationTray/LibraryNavigationTray.vue'
 import WithLoader from '../../components/ui/WithLoader.vue'
@@ -9,11 +10,7 @@ const { active, anonymous } = useSession()
 </script>
 <template>
   <div id="LibraryIndexView">
-    <cyan-top-app-bar>
-      <h2>
-        {{ $t('library.games.title') }}
-      </h2>
-    </cyan-top-app-bar>
+    <cn-app-bar :title="$t('library.games.title')" />
     <WithLoader
       large
       :suspended="!active"
@@ -25,5 +22,6 @@ const { active, anonymous } = useSession()
         </main>
       </WithPermission>
     </WithLoader>
+    <FabTray />
   </div>
 </template>
