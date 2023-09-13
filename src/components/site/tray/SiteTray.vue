7cc9e7821d9630a2a1bb2e25d4c6aa84f7ae0673
Ville Takanen <vitkukissa@gmail.com>
2023-09-13 09:59:40 +0300

    fix: linter

--- src/components/site/tray/SiteTray.vue
+++ src/components/site/tray/SiteTray.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { useScreenSize } from '../../../composables/useScreenSize';
+import { useScreenSize } from '../../../composables/useScreenSize'
 import PageIndex from '../../SiteTray/PageIndex.vue'
 import SiteTrayHeader from './SiteTrayHeader.vue'
 import { computed, ref } from 'vue'
@@ -12,7 +12,10 @@ const trayVisible = computed(() => showTray.value || !isSmall.value)
 
 <template>
   <article class="small column surface">
-    <SiteTrayHeader @show="(visible) => showTray = visible"/>
-    <PageIndex v-if="trayVisible" :class="{ 'rise-a': isSmall, 'p-1': isSmall }"/>
+    <SiteTrayHeader @show="(visible) => showTray = visible" />
+    <PageIndex
+      v-if="trayVisible"
+      :class="{ 'rise-a': isSmall, 'p-1': isSmall }"
+    />
   </article>
 </template>
