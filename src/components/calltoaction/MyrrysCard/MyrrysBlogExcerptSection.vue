091d74a9b49da0aba807662cadfcb1616102c3da
Ville Takanen <vitkukissa@gmail.com>
2023-11-10 15:07:33 +0200

    fix: myrrys blog rss redirect

--- src/components/calltoaction/MyrrysCard/MyrrysBlogExcerptSection.vue
+++ src/components/calltoaction/MyrrysCard/MyrrysBlogExcerptSection.vue
@@ -1,28 +1,51 @@
 <script lang="ts" setup>
 import { onMounted, ref } from 'vue'
 import WithLoader from '../../ui/WithLoader.vue'
+import { logDebug } from '../../../utils/logHelpers'
 
 interface WordpressArticle {
-  ID: number
-  URL: string
+  guid: number
+  link: string
   title: string
-  date: string
-  excerpt: string
+  pubDate: string
+  description: string
 }
 const articles = ref<WordpressArticle[]>([])
 const loading = ref(true)
 
 async function fetchArticles () {
   // const after = '?after=' + new Date(Date.now() - (1000 * 3600 * 24) * 60).toISOString().substring(0, 10)
-  const url = 'https://public-api.wordpress.com/rest/v1.1/sites/myrrys.com/posts?number=3'
+  const url = 'https://www.myrrys.com/rss.xml'
   try {
     const response = await fetch(url)
-    const json = await response.json()
+    // the response is an XML document with a root element <rss><channel>...</channel></rss>
+    const xml = await response.text()
+    logDebug('myrrysCard', 'fetchArticles', 'xml', xml)
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(xml, 'application/xml')
+    const items = doc.getElementsByTagName('item')
+    logDebug('myrrysCard', 'fetchArticles', 'items', items)
+    for (let i = 0; i < items.length && i < 3; i++) {
+      const item = items[i]
+      const title = item.getElementsByTagName('title')[0].textContent
+      const link = item.getElementsByTagName('link')[0].textContent
+      const description = item.getElementsByTagName('description')[0].textContent
+      const pubDate = item.getElementsByTagName('pubDate')[0].textContent
+      articles.value.push({
+        guid: i,
+        title: title || '',
+        link: link || '',
+        description: description || '',
+        pubDate: pubDate || ''
+      })
+    }
+
+    /*const json = await response.blob
     // logDebug('LokiCard', 'fetchArticles', 'json', json)
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     articles.value = json.posts.map((article:unknown) => {
       return article as WordpressArticle
-    })
+    })*/
     loading.value = false
   } catch (error) {
     console.error(error)
@@ -34,13 +57,14 @@ onMounted(() => {
   fetchArticles()
 })
 
-function excerptFrom(article:WordpressArticle) {
+/*function excerptFrom(article:WordpressArticle) {
   const element = document.createElement('div')
-  element.innerHTML = article.excerpt
-  const content = element.textContent
-  const ellipsis = content && content.length > 120 ? '...' : ''
-  return content?.substring(0, 140) + ellipsis
-}
+  element.innerHTML = article.description
+  // const content = element.textContent
+  //const ellipsis = content && content.length > 120 ? '...' : ''
+  //return content?.substring(0, 140) + ellipsis
+
+}*/
 
 </script>
 
@@ -49,9 +73,9 @@ function excerptFrom(article:WordpressArticle) {
     <WithLoader :suspended="!articles || articles.length < 1">
       <a
         v-for="article in articles"
-        :key="article.ID"
+        :key="article.guid"
         class="intemlink hoverable clickable"
-        :href="article.URL"
+        :href="article.link"
         target="_top"
       >
         <h4
@@ -60,7 +84,7 @@ function excerptFrom(article:WordpressArticle) {
           :innerHTML="article.title"
         />
         <p class="TypeCaption">
-          {{ excerptFrom(article) }}
+          {{ article.description }}
         </p>
       </a>
     </WithLoader>
